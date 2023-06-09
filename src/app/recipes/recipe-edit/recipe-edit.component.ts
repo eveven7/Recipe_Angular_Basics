9c2dcaef4d498cb51bc0059bf277290533ba2ece
Evelina <evelina.venckute@swedbank.lt>
2023-06-09 15:21:10 +0300

    finished

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -96,4 +96,9 @@ export class RecipeEditComponent implements OnInit {
   onCancel() {
     this.router.navigate(['../'], { relativeTo: this.route });
   }
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    // (<FormArray>this.recipeForm.get('ingredients')).clear();
+
+  }
 }
